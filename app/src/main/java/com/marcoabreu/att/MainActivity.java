8820043d887da3e6461e864c64fc45e06399bacd
MarcoAbreu <Marco.G.Abreu+github@gmail.com>
2016-06-23 14:32:30 +0200

    Communication between host and DOT working

--- app/src/main/java/com/marcoabreu/att/MainActivity.java
+++ app/src/main/java/com/marcoabreu/att/MainActivity.java
@@ -1,13 +1,270 @@
 package com.marcoabreu.att;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Date;
+import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
+    TextView info, infoip, msg;
+    String message = "";
+    ServerSocket serverSocket;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        info = (TextView) findViewById(R.id.textViewInfo);
+        infoip = (TextView) findViewById(R.id.textViewInfoIp);
+        msg = (TextView) findViewById(R.id.textViewMsg);
+
+        Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.marcoabreu.att/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.marcoabreu.att/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    private class SocketServerThread extends Thread {
+
+        static final int SocketServerPORT = 12022;
+        int count = 0;
+
+        @Override
+        public void run() {
+            try {
+                serverSocket = new ServerSocket(SocketServerPORT);
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        info.setText("I'm waiting here: "
+                                + serverSocket.getLocalPort());
+                    }
+                });
+
+//                while (true) {
+//                    Socket socket = serverSocket.accept();
+//                    count++;
+//                    message += "#" + count + " from " + socket.getInetAddress()
+//                            + ":" + socket.getPort() + "\n";
+//
+//                    MainActivity.this.runOnUiThread(new Runnable() {
+//
+//                        @Override
+//                        public void run() {
+//                            msg.setText(message);
+//                        }
+//                    });
+//
+//                    SocketServerReplyThread socketServerReplyThread = new SocketServerReplyThread(
+//                            socket, count);
+//                    socketServerReplyThread.run();
+//
+//                }
+
+                while(true) {
+                    MainActivity.this.runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            infoip.setText("Waiting for connection");
+                            msg.setText("No connection");
+                        }
+                    });
+
+                    Socket socket = serverSocket.accept();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            infoip.setText("Connected at " + new Date().toString());
+                            msg.setText("Waiting for messages...");
+                        }
+                    });
+
+                    try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+                        while (!socket.isClosed()) {
+                            final String curMsg = in.readLine();
+
+                            if(curMsg == null) { //Connection closed
+                                break;
+                            }
+
+                            MainActivity.this.runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    msg.setText("Received message: '" + curMsg + "' at " + new Date().toString());
+                                }
+                            });
+                        }
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private class SocketServerReplyThread extends Thread {
+
+        private Socket hostThreadSocket;
+        int cnt;
+
+        SocketServerReplyThread(Socket socket, int c) {
+            hostThreadSocket = socket;
+            cnt = c;
+        }
+
+        @Override
+        public void run() {
+            OutputStream outputStream;
+            String msgReply = "Hello from Android, you are #" + cnt;
+
+            try {
+                outputStream = hostThreadSocket.getOutputStream();
+                PrintStream printStream = new PrintStream(outputStream);
+                printStream.print(msgReply);
+                printStream.close();
+
+                message += "replayed: " + msgReply + "\n";
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        msg.setText(message);
+                    }
+                });
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                message += "Something wrong! " + e.toString() + "\n";
+            }
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    msg.setText(message);
+                }
+            });
+        }
+
+    }
+
+    private String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "SiteLocalAddress: "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+
+                }
+
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
     }
 }
--- hostapplication/src/main/java/com/marcoabreu/att/host/HostApp.java
+++ hostapplication/src/main/java/com/marcoabreu/att/host/HostApp.java
@@ -1,27 +1,76 @@
 package com.marcoabreu.att.host;
 
-import java.io.Console;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.List;
 
+import se.vidstige.jadb.AdbServerLauncher;
 import se.vidstige.jadb.JadbConnection;
 import se.vidstige.jadb.JadbDevice;
 import se.vidstige.jadb.JadbException;
+import se.vidstige.jadb.Transport;
+import se.vidstige.jadb.managers.Package;
+import se.vidstige.jadb.managers.PackageManager;
 
 public class HostApp {
     public static void main(String args[]) {
         System.out.println("Test");
 
+        //establish connection and start the app
         try {
+            new AdbServerLauncher().launch();
             JadbConnection jadb = new JadbConnection();
             List<JadbDevice> devices = jadb.getDevices();
 
             for(JadbDevice device : devices) {
                 System.out.println(device.toString());
             }
+
+            JadbDevice curDevice = devices.get(0);
+
+
+            Transport transport = jadb.createTransport();
+            transport.send(String.format("host-serial:%s:forward:tcp:%d;tcp:%d", curDevice.getSerial(), 12022, 12022));
+            transport.verifyResponse();
+
+            //Launch app
+            PackageManager pm = new PackageManager(curDevice);
+            pm.launch(new Package("com.marcoabreu.att"));
+            Thread.sleep(5000);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (JadbException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        try (
+                Socket echoSocket = new Socket("127.0.0.1", 12022);
+                PrintWriter out =
+                        new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in =
+                        new BufferedReader(
+                                new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn =
+                        new BufferedReader(
+                                new InputStreamReader(System.in))
+        ){
+            System.out.println("Enter your message:");
+            String curMsg = "";
+
+            while((curMsg = stdIn.readLine()).compareToIgnoreCase("exit") != 0) {
+                out.write(curMsg + "\r\n");
+                out.flush();
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
